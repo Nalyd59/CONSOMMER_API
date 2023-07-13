0f41cf49c0d403f02528ecc8685b3fb4ba10119e
Nalyd59 <dy.crepin@gmail.com>
2023-07-13 15:02:53 +0200

    maj

--- js/script.js
+++ js/script.js
@@ -1,4 +1,5 @@
 // Votre code Javascript ici
+
 window.onload = function () {
     let requestGET = async function(url){
         const requestGET = new Request(url,{
@@ -33,12 +34,3 @@ if (!res) {
 fetch('http://reqres.in/api/users?per_page=+12')
     .then(r => r.json())
     .then(data => console.log(data.data[0].id))*/
-
-
-
-git init
-git add .
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/Nalyd59/CONSOMMER_API.git
-git push -u origin main
